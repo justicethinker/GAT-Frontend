4a3dcefc629d35472fc074724650ba76ad6a4442
Ben-W3Cloud <bcbandit17@gmail.com>
2026-05-15 22:03:44 +0100

    changed interval

--- client/src/pages/Arbitrage.tsx
+++ client/src/pages/Arbitrage.tsx
@@ -235,8 +235,8 @@ function useArbitrageScanner(
       updateCountRef.current += 1;
       console.log("[WS] Message counter:", updateCountRef.current);
 
-      if (updateCountRef.current >= 5) {
-        console.log("[WS] Clearing table after 5 updates.");
+      if (updateCountRef.current >= 2) {
+        console.log("[WS] Clearing table after 2 updates.");
         updateCountRef.current = 0;
         setFoundOpps([]);
         return;
